4ea4eeeee82a5545233b7b3d2e6b497a46fa0217
tktb-tess <chemusiclove6174@gmail.com>
2026-01-12 15:38:33 +0900

    heha

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
