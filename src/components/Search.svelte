d88c244c08de4d7aa426c944a489bbfbd12435ca
tktb-tess <chemusiclove6174@gmail.com>
2026-01-27 14:37:05 +0900

    aaa

---
+++ src/components/Search.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  type SearchMode = 'former' | 'latter' | 'exact' | 'partial';
+  let searchWord = $state('');
+  let searchMode = $state<SearchMode>('former');
+</script>
+
+<input type="text" bind:value={searchWord} />
+<input type="radio" name="" id="radio-1" />
+<label for="radio-1">前方</label>
+
+<style lang="postcss">
+  @reference '../styles/globals.css';
+</style>
