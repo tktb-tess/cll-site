00a34da1966580ce1a1dceb9abdec5ab3bc8754a
tktb-tess <chemusiclove6174@gmail.com>
2026-01-28 11:09:29 +0900

    glossary

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,12 +1,70 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   type SearchMode = 'former' | 'latter' | 'exact' | 'partial';
+  type WordData = {
+    word: string;
+    contents: string[];
+  };
   let searchWord = $state('');
   let searchMode = $state<SearchMode>('former');
+  let wordData: WordData[] | undefined = $state();
+
+  onMount(async () => {
+    wordData = (await import('../modules/glossary.json')).default;
+  });
+
+  $effect(() => {
+    const newUrl = new URL(document.URL);
+    newUrl.searchParams.set('mode', searchMode);
+    if (searchWord) {
+      newUrl.searchParams.set('word', searchWord);
+    } else {
+      newUrl.searchParams.delete('word');
+    }
+    window.history.replaceState(null, '', newUrl);
+  });
 </script>
 
-<input type="text" bind:value={searchWord} />
-<input type="radio" name="" id="radio-1" />
-<label for="radio-1">前方</label>
+<section
+  aria-labelledby="kensaku"
+  class="flex flex-col gap-1 w-full max-w-80 mx-auto"
+>
+  <h3 id="kensaku" class="text-center">検索</h3>
+  <input type="text" class="block w-full" bind:value={searchWord} />
+  <div class="flex justify-center gap-2">
+    <span>一致方式:</span>
+    <input
+      type="radio"
+      name="search-mode"
+      id="radio-1"
+      onclick={() => (searchMode = 'former')}
+      checked
+    />
+    <label for="radio-1">前方</label>
+    <input
+      type="radio"
+      name="search-mode"
+      id="radio-2"
+      onclick={() => (searchMode = 'latter')}
+    />
+    <label for="radio-2">後方</label>
+    <input
+      type="radio"
+      name="search-mode"
+      id="radio-3"
+      onclick={() => (searchMode = 'exact')}
+    />
+    <label for="radio-3">完全</label>
+    <input
+      type="radio"
+      name="search-mode"
+      id="radio-4"
+      onclick={() => (searchMode = 'partial')}
+    />
+    <label for="radio-4">部分</label>
+  </div>
+</section>
 
 <style lang="postcss">
   @reference '../styles/globals.css';
